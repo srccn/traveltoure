a0fee56692e284488776762aebb099b82477e8ba
fang-frank2020 <frankfangfrank@gmail.com>
2023-06-10 20:02:29 -0400

    separated server from front-end

--- server/src/index.js
+++ server/src/index.js
@@ -27,9 +27,7 @@ app.use((req, res, next) => {
     res.set("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE');
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
-})
-
-app.use(express.static(path.join(__dirname, "..", "..", "front-end", "build")));
+});
 
 app.listen(PORT, () => {console.log("Listening on port " + PORT)});
 
