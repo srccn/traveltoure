1e92891419617a569267d9f6bef1d8dd98f454cf
fang-frank2020 <frankfangfrank@gmail.com>
2023-06-10 23:13:42 -0400

    changed

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(session({
 
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", PORT === 3001 ? "http://localhost:3001" : "https://traveltour.vercel.app");
+    res.header("Access-Control-Allow-Origin", PORT === 3001 ? "http://localhost:3000" : "https://traveltour-frontend.onrender.com");
     res.header("Access-Control-Allow-Headers", "*");
     res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE');
@@ -123,6 +123,7 @@ app.post("/api/login", (req, res, next) => {
             res.cookie("name", username, {
                 maxAge: 3600000,
                 httpOnly: false,
+                domain: ".onrender.com"
             });
             res.send({status: "logged in", data: req.sessionID});
         }
